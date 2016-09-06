dafa0882194d611c7678e29fd961988374a79f8c
Go <gaojie@limei.com>
2016-09-06 18:11:15 +0800

    update

--- offer_6/main.cpp
+++ offer_6/main.cpp
@@ -5,6 +5,8 @@ using namespace std;
 class Solution{
 public:
     int minNumberInRotateArray(vector<int> rotateArray) {
+        if(rotateArray.empty()) return 0;
+        int *left, *right;
 
     }
 };
